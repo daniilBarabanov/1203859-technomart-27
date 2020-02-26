a3ad31f74109bdf9b895927270d6f80eacd7d080
daniilBarabanov <55635367+daniilBarabanov@users.noreply.github.com>
2020-02-26 17:44:25 +0300

    mnogo chego
    mnogo chego

--- js/script.js
+++ js/script.js
@@ -57,3 +57,28 @@ window.addEventListener("keydown", function(evt) {
         }
     }
 })
+
+
+var mapLink = document.querySelector(".route");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+mapLink.addEventListener("click", function(evt){
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+});
+mapClose.addEventListener("click", function(evt){
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function(evt){
+    if (evt.keyCode === 27) {
+        if (mapPopup.classList.contains("modal-show")) {
+            evt.preventDefault();
+            mapPopup.classList.remove("modal-show");
+        }
+    }
+});
+
+
