30d990ae95bbf80fa948d4b27206a03b714c07cd
daniilBarabanov <55635367+daniilBarabanov@users.noreply.github.com>
2020-02-27 20:32:38 +0300

    okna
    okna

--- js/script.js
+++ js/script.js
@@ -82,7 +82,8 @@ window.addEventListener("keydown", function(evt) {
 });
 
 
-var buyLink = document.querySelectorAll(".button-buy"), index, button;
+var buyLink = document.querySelectorAll(".button-buy"),
+    index, button;
 var buyPopup = document.querySelector(".pop-buy");
 var buyClose = buyPopup.querySelector(".modal-close");
 var buyCont = buyPopup.querySelector(".continue");
@@ -90,24 +91,24 @@ var buyCont = buyPopup.querySelector(".continue");
 for (index = 0; index < buyLink.length; index++) {
     button = buyLink[index];
 
-button.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    buyPopup.classList.add("modal-show");
-});
-buyClose.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    buyPopup.classList.remove("modal-show");
-});
-buyCont.addEventListener("click", function(evt){
-    evt.preventDefault();
-    buyPopup.classList.remove("modal-show");
-});
-window.addEventListener("keydown", function(evt) {
-    if (evt.keyCode === 27) {
-        if (buyPopup.classList.contains("modal-show")) {
-            evt.preventDefault();
-            buyPopup.classList.remove("modal-show");
+    button.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        buyPopup.classList.add("modal-show");
+    });
+    buyClose.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        buyPopup.classList.remove("modal-show");
+    });
+    buyCont.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        buyPopup.classList.remove("modal-show");
+    });
+    window.addEventListener("keydown", function(evt) {
+        if (evt.keyCode === 27) {
+            if (buyPopup.classList.contains("modal-show")) {
+                evt.preventDefault();
+                buyPopup.classList.remove("modal-show");
+            }
         }
-    }
-});
+    });
 }
