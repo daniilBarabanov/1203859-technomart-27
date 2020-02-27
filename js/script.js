3f67e5f8fc70724e0a91592d240d04f3bba231eb
daniilBarabanov <55635367+daniilBarabanov@users.noreply.github.com>
2020-02-27 12:44:53 +0300

    js
    js

--- js/script.js
+++ js/script.js
@@ -82,15 +82,23 @@ window.addEventListener("keydown", function(evt) {
 });
 
 
-var buyLink = document.querySelector(".button-buy");
+var buyLink = document.querySelectorAll(".button-buy"), index, button;
 var buyPopup = document.querySelector(".pop-buy");
 var buyClose = buyPopup.querySelector(".modal-close");
+var buyCont = buyPopup.querySelector(".continue");
 
-buyLink.addEventListener("click", function(evt) {
+for (index = 0; index < buyLink.length; index++) {
+    button = buyLink[index];
+
+button.addEventListener("click", function(evt) {
     evt.preventDefault();
     buyPopup.classList.add("modal-show");
 });
 buyClose.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    buyPopup.classList.remove("modal-show");
+});
+buyCont.addEventListener("click", function(evt){
     evt.preventDefault();
     buyPopup.classList.remove("modal-show");
 });
@@ -102,3 +110,4 @@ window.addEventListener("keydown", function(evt) {
         }
     }
 });
+}
